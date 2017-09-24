8feea3643554b3466209afdc340d1151dc07e83e
Mitra Ardron <mitra@mitra.biz>
2017-09-24 02:32:48 +0000

    Changes made on server

--- scripts/run_dweb_me.sh
+++ scripts/run_dweb_me.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PIPS="py-dateutil redis base58 pynacl"
+PIPS="py-dateutil redis base58 pynacl multihssh"
 cd /usr/local/dweb_gateway
 pip install --disable-pip-version-check -U $PIPS
 if git commit -a -m "Changes made on server"
