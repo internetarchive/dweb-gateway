882a517307e17d256032c823d404abeb7ffc2b95
Mitra Ardron <mitra@mitra.biz>
2017-09-26 00:41:25 +0000

    Changes made on server

--- scripts/run_dweb_me.sh
+++ scripts/run_dweb_me.sh
@@ -2,7 +2,7 @@
 
 PIPS="multihash py-dateutil redis base58 pynacl"
 cd /usr/local/dweb_gateway
-pip install --disable-pip-version-check -U $PIPS
+pip3 install --disable-pip-version-check -U $PIPS
 [ -d data ] || mkdir data
 if git commit -a -m "Changes made on server"
 then
@@ -20,13 +20,13 @@ then
 	curl -L -o data/idents_files_urls_sqlite.gz https://archive.org/download/ia_papers_manifest_20170919/index/idents_files_urls.sqlite.gz
 	gunzip data/idents_files_urls_sqlite.gz
 fi
-cd python
+#cd python
 if ps -ef | grep ServerGateway | grep -v grep
 then
 	echo "You need to kill that process above first"
 else
     echo "Starting Server "
-    python -m ServerGateway &
+    python3 -m python.ServerGateway
     ps -ef | grep ServerGateway | grep -v grep
 fi
 
