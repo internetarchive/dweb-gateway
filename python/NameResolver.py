ed4ba359d496e8bc08e40ce660f0259ca941bb1a
Mitra Ardron <mitra@mitra.biz>
2017-12-19 05:35:57 +0000

    Changes made on server

--- python/NameResolver.py
+++ python/NameResolver.py
@@ -198,7 +198,7 @@ class NameResolverFile(NameResolver):
             #This next line is to get around bug in IPFS propogation
             #See https://github.com/ipfs/js-ipfs/issues/1156
             ipfsgatewayurl = "https://ipfs.io/ipfs/{}".format(ipldhash)
-            res = requests.get(ipfsgatewayurl); # Going to ignore the result
+            res = requests.head(ipfsgatewayurl); # Going to ignore the result
             logging.debug("XXX@202 - ran priming process on ipfs.io to work around JS-IPFS issue #1156")
         LocationService.set(self.multihash.multihash58, url, verbose=verbose)
         return {"ipldhash": ipldhash}
