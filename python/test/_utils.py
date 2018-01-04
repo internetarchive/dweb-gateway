bfa66827e5afd175c15640b1678fbba347009953
Mitra Ardron <mitra@mitra.biz>
2018-01-04 14:21:14 -0800

    Fix unit tests

--- python/test/_utils.py
+++ python/test/_utils.py
@@ -1,10 +1,11 @@
 from python.ServerGateway import DwebGatewayHTTPRequestHandler
 
 
-def _processurl(url, verbose, **kwargs):
+def _processurl(url, verbose, headers={}, **kwargs):
     # Simulates HTTP Server process - wont work for all methods
     args = url.split('/')
     method = args.pop(0)
+    DwebGatewayHTTPRequestHandler.headers = headers # This is a kludge, put headers on class, method expects an instance.
     f = getattr(DwebGatewayHTTPRequestHandler, method)
     assert f
     namespace = args.pop(0)
