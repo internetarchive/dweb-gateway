0b499f6371e638299fca17ebfca58538b89f6f8b
Mitra Ardron <mitra@mitra.biz>
2018-01-27 00:11:50 +0000

    Changes made on server

--- python/HashResolvers.py
+++ python/HashResolvers.py
@@ -1,11 +1,12 @@
 import logging
 from .NameResolver import NameResolverFile
-from .miscutils import httpget
+from .miscutils import loads, dumps, httpget
 from .Errors import CodingException, NoContentException
 from .HashStore import LocationService, MimetypeService
 from .LocalResolver import LocalResolverFetch
 from .Multihash import Multihash
 from .DOI import DOIfile
+from .Archive import ArchiveFile
 
 class HashResolver(NameResolverFile):
     """
@@ -65,7 +66,7 @@ class HashResolver(NameResolverFile):
             if verbose: logging.debug("No URL, looking for DOI file for {0}.{1}".format(namespace,hash))   
             #!SEE-OTHERHASHES -this is where we look things up in the DOI.sql etc essentially cycle through some other classes, asking if they know the URL
             # ch = DOIfile(multihash=ch.multihash).url  # Will fill in url if known. Note will now return a DOIfile, not a Sha1Hex
-            ch = this.searcharchivefor(verbose=verbose) # Will now be a ArchiveFile
+            ch = ch.searcharchivefor(verbose=verbose) # Will now be a ArchiveFile
         if ch.url.startswith("local:"):
             ch = LocalResolverFetch.new("rawfetch", hash, **kwargs)
         if not (ch and ch.url):
@@ -100,19 +101,22 @@ class HashResolver(NameResolverFile):
         else:
             return httpget(self.url)
 
-    def searcharchivefor(self, multihash, verbose=False, **kwargs):
+    def searcharchivefor(self, multihash=None, verbose=False, **kwargs):
         # Note this only works on certain machines
         # And will return a ArchiveFile
-        searchurl = "http://archive.org/services/dwhf.php?key=sha1&val={}".format((multihash or this.multihash).sha1hex)
-        res = httpget(self.url)
-        if res["error"]:
+        searchurl = "http://archive.org/services/dwhf.php?key=sha1&val={}".format((multihash or self.multihash).sha1hex)
+        res = loads(httpget(searchurl))
+        logging.info("XXX@searcharchivefor res={}".format(res))
+        if res.get("error"):
             # {"error": "internal use only"}
             raise ForbiddenException(what="SHA1 search from machine unless its whitelisted by Aaron")
         if not res["hits"]["total"]:
             # {"key":"sha1","val":"88d4b0d91acd3c25139804afbf4aef4e675bef63","hits":{"total":0,"matches":[]}}
             raise NoContentException()
         # {"key": "sha1", "val": "88...2", "hits": {"total": 1, "matches": [{"identifier": ["<ITEMID>"],"name": ["<FILENAME>"]}]}}
-        return ArchiveFile.new("archiveid", res["hits"][0]["identifier"], res["hits"][0]["name"], verbose=verbose)
+        firstmatch = res["hits"]["matches"][0]
+        logging.info("ArchiveFile.new({},{},{}".format("archiveid", firstmatch["identifier"][0], firstmatch["name"][0]))
+        return ArchiveFile.new("archiveid", firstmatch["identifier"][0], firstmatch["name"][0], verbose=verbose)
 
     def content(self, verbose=False, **kwargs):
         """
